e41d1656399b243a7d0ed9d59e2c9019ecb72eba
inzaide <zwsayage@gmail.com>
2017-06-25 00:04:05 -0700

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ const log = message => {
 };
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
+client.playlists = new Discord.Collection();
+fs.readdir('./events/', (err, files) => {
+  if (err) console.error(err);
+  log(`Loading a total of ${files.length} events.`);
+};
 fs.readdir('./commands/', (err, files) => {
   if (err) console.error(err);
   log(`Loading a total of ${files.length} commands.`);
